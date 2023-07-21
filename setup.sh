13c075056f9e63191e9109cd18256c3d1a401dd9
dencoseca <leonbrown@Leons-MacBook-Pro-2.local>
2023-07-21 22:30:24 +0100

    style docker scripts

--- setup.sh
+++ setup.sh
@@ -96,16 +96,97 @@ sed -i '' 's/# zstyle \x27:omz:update\x27 mode auto/zstyle \x27:omz:update\x27 m
 
 print_loading_message
 
+echo 'creating style source file...'
+cat <<EOM >>~/.styles.sh
+# Reset
+RESET='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
+EOM
+
 echo 'adding custom shell setup to .zshrc...'
 cat <<EOM >>~/.zshrc
 
-############
-## COLORS ##
-############
+###########
+## STYLE ##
+###########
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-RESET='\033[0m'
+source ~/.styles.sh
+
+print_message() {
+  local STRING="$1"
+  local STYLE="$2"
+
+  echo -e "${STYLE}${STRING}${RESET}"
+}
 
 ################
 ## TOOL SETUP ##
@@ -150,10 +231,10 @@ docker_sc() {
   local CONTAINER_NAMES
   CONTAINER_NAMES=$(docker ps -a --format "{{.Names}}")
   if [[ -n "$CONTAINER_NAMES" ]]; then
-    echo "${GREEN}STOPPING CONTAINERS${RESET}"
+    print_message "STOPPING CONTAINERS" "$BGreen"
     echo "$CONTAINER_NAMES" | xargs -r docker stop
   else
-    echo "${RED}No CONTAINERS to STOP${RESET}"
+    print_message "No CONTAINERS to STOP" "$BRed"
   fi
 }
 
@@ -161,10 +242,10 @@ docker_rc() {
   local CONTAINER_NAMES
   CONTAINER_NAMES=$(docker ps -a --format "{{.Names}}")
   if [[ -n "$CONTAINER_NAMES" ]]; then
-    echo "${GREEN}REMOVING CONTAINERS${RESET}"
+    print_message "REMOVING CONTAINERS" "$BGreen"
     echo "$CONTAINER_NAMES" | xargs -r docker rm
   else
-    echo "${RED}No CONTAINERS to REMOVE${RESET}"
+    print_message "No CONTAINERS to REMOVE" "$BRed"
   fi
 }
 
@@ -172,10 +253,10 @@ docker_rv() {
   local VOLUME_NAMES
   VOLUME_NAMES=$(docker volume ls -q)
   if [[ -n "$VOLUME_NAMES" ]]; then
-    echo "${GREEN}REMOVING VOLUMES${RESET}"
+    print_message "REMOVING VOLUMES" "$BGreen"
     echo "$VOLUME_NAMES" | xargs -r docker volume rm
   else
-    echo "${RED}No VOLUMES to REMOVE${RESET}"
+    print_message "No VOLUMES to REMOVE" "$BRed"
   fi
 }
 
