6a292d7f6eda3d16b6c364f12eabd14fc6e0aeed
dencoseca <leonbrown@Leons-MacBook-Pro-2.local>
2024-01-07 00:58:04 +0000

    more

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ print_message() {
   say "$1"
 }
 
-ignore_dave_and_leave_him_in_space_to_suffocate_that_will_teach_the_conniving_bastard() {
+ignore_dave_and_leave_him_in_space_to_suffocate_the_stupid_jerk_that_will_teach_him() {
   print_message "...I'm sorry Dave, I'm afraid I can't do that"
 }
 
@@ -66,7 +66,7 @@ stop_spinner() {
   echo "${spinner_message}... done!"
 }
 
-trap 'ignore_dave_and_leave_him_in_space_to_suffocate_that_will_teach_the_conniving_bastard' ERR
+trap 'ignore_dave_and_leave_him_in_space_to_suffocate_the_stupid_jerk_that_will_teach_him' ERR
 trap 'stop_spinner' EXIT
 
 cd ~ || exit 1
