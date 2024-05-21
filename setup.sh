e8af35696c7ca91524f8fa164bb5bc09a246b6b3
dencoseca <leonbrown@Leons-MacBook-Pro-2.local>
2024-05-21 23:14:17 +0100

    Remove Warp themes setup from configuration script
    This commit removes the Warp themes setup from the setup.sh script, as it was unnecessary and causing confusion. The change simplifies the setup process by eliminating the need to configure Warp Themes, which were not actually used in the application.

--- setup.sh
+++ setup.sh
@@ -233,9 +233,6 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-# warp themes
-WARP_THEMES_DIR="$HOME/.warp/themes"
-
 # bat theme
 export BAT_THEME='zenburn'
 
