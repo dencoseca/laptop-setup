bc96074dedfd71ec3daab132636ac04ee282a33b
dencoseca <leonbrown@Leons-MacBook-Pro-2.local>
2024-06-01 09:22:45 +0100

    Removed 'bat' setup from setup.sh
    The 'bat' installation and setup was removed in the 'setup.sh' script. The relevant code relating to the installation and configuration (specifically, the BAT_THEME environmental variable) of 'bat' was removed.

--- setup.sh
+++ setup.sh
@@ -157,7 +157,6 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 print_message 'Creating Brewfile'
 cat << 'EOF' > "$HOME/Brewfile"
 # formulae
-brew "bat"
 brew "htop"
 brew "jq"
 brew "watch"
@@ -243,9 +242,6 @@ eval "$(starship init zsh)"
 export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
-# bat
-export BAT_THEME='zenburn'
 
 ################################
 ## CUSTOM ALIASES / FUNCTIONS ##
